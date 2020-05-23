a4c27aee8626fb12903b0ad628b57e4421cda66c
Tran <tutran1@kpmg.com>
2020-05-23 03:09:35 +0200

    initial commit

---
+++ build/precache-manifest.02636a6ba9d9ef301d0a0ed37ce33ea2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17143b74cdfab77a2d43e8ed60f1ee54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "894ab4c78c14c9c7947a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c108e79ed80b10780693",
+    "url": "/static/js/2.c30ebfa6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c30ebfa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "894ab4c78c14c9c7947a",
+    "url": "/static/js/main.352b76aa.chunk.js"
+  },
+  {
+    "revision": "ac8cd2ffb2a92fe3624b",
+    "url": "/static/js/runtime-main.3e4898a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
